2036513ccdce395aa5640f849470e9cc899355e7
kerli421 <kerli421@proton.me>
2024-09-19 21:16:34 +1200

    added hangman

--- include/hangman.h
+++ include/hangman.h
@@ -1,61 +1,161 @@
-#pragma once
+﻿#pragma once
 
 #include <string>
 #include <vector>
+#include <set>
+
+using namespace std;
 
 class Hangman {
 
-    std::vector<String>
+    const int MAX_STAGE = 7;
 
-    hangmen {"+ -- - +\n|   |\n        |\n        |\n        |\n        |\n        ======== = ",
-        + -- - +
-        |   |
-        O   |
-        |
-        |
-        |
-        ======== = ''', '''
-        + -- - +
-        |   |
-        O   |
-        |   |
-        |
-        |
-        ======== = ''', '''
-        + -- - +
-        |   |
-        O   |
-        /|   |
-        |
-        |
-        ======== = ''', '''
-        + -- - +
-        |   |
-        O   |
-        /|\  |
-        |
-        |
-        ======== = ''', '''
-        + -- - +
-        |   |
-        O   |
-        /|\  |
-        /    |
-        |
-        ======== = ''', '''
-        + -- - +
-        |   |
-        O   |
-        /|\  |
-        / \  |
-        |
-        ======== = '''};
-
+    std::set<char> seen_letters;
+       
 	public:
 		void startGame();
 
 	private:
-		void display();
-};
+		void display(int stage);
+
+        std::vector<std::string> hangman_stages = {
+            R"(
+                   +---+
+                   |   |
+                       |
+                       |
+                       |
+                       |
+                =========
+            )",
+
+            R"(
+                   +---+
+                   |   |
+                   O   |
+                       |
+                       |
+                       |
+                =========
+            )",
+
+            R"(
+                   +---+
+                   |   |
+                   O   |
+                   |   |
+                       |
+                       |
+                =========
+            )",
+
+            R"(
+                   +---+
+                   |   |
+                   O   |
+                  /|   |
+                       |
+                       |
+                =========
+            )",
+
+            R"(
+                   +---+
+                   |   |
+                   O   |
+                  /|\  |
+                       |
+                       |
+                =========
+            )",
+
+            R"(
+                   +---+
+                   |   |
+                   O   |
+                  /|\  |
+                  /    |
+                       |
+                =========
+            )",
+
+            R"(
+                   +---+
+                   |   |
+                   O   |
+                  /|\  |
+                  / \  |
+                       |
+                =========
+            )"
+        };
+
+        std::vector<std::string> flower_stages = {
+             R"(
+                 _
+               _(_)_ 
+              (_)@(_)
+                (_)
+                | |
+              ======
+             )",  // Full healthy flower
+
+            R"(
+                 _
+               _(_)_ 
+              (_)@(_)
+                (_)
+                | 
+              ======
+             )",  // One leaf missing
+
+            R"(
+                 _
+               _(_)_ 
+              (_)@(_)
+                (_)
+        
+              ======
+             )",  // No leaves
+
+            R"(
+                 _
+               _(_)_ 
+              (_)@(_)
+        
+      
+              ======
+             )",  // Flower stem only
+
+            R"(
+                 _
+               _(_)_ 
+        
+        
+      
+              ======
+             )",  // Just the upper part of the stem
+
+            R"(
+                 _
+         
+        
+        
+      
+              ======
+             )",  // Top of the stem gone
+
+            R"(
+         
+         
+        
+        
+      
+              ======
+            )"  // Completely wilted (game over)
+        };
+
+
+        
 
 
--- src/hangman.cpp
+++ src/hangman.cpp
@@ -1,16 +1,99 @@
 #include <stdio.h>
 #include <iostream> 
+#include <chrono>
+#include <sstream>
+#include <thread>
 
 #include "hangman.h"
+#include "get_data.h"
 
 
-void Hangman::display() {
-	std::cout << "O\n---\n|\n" << "/\\" << std::endl;
+void Hangman::display(int stage) {
+	// system("CLS"); // Clear the terminal screen
+	std::cout << R"(
+
+
+	   +===================================================+
+	   |   _   _                                           |
+	   |  | | | | __ _ _ __   __ _ _ __ ___   __ _ _ __    |
+	   |  | |_| |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \   |
+	   |  |  _  | (_| | | | | (_| | | | | | | (_| | | | |  |
+	   |  |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  |
+	   |                     |___/                         |
+	   +===================================================+
+
+
+	)" << std::endl;
+
+	vector<string> seen_letters_box;
+	seen_letters_box.push_back("+---------+");
+
+	vector<char> temp;
+	for (const char& seen_letter : seen_letters) {
+		temp.push_back(seen_letter);
+
+		if (temp.size() >= 4) {
+			string temp2 = "| ";
+			for each (char c in temp)
+			{
+				temp2 += (c + " ");
+			}
+			temp2 += "|";
+			seen_letters_box.push_back(temp2);
+		}
+	}
+
+	//std::cout << hangman_stages[stage] << std::endl;
+	std::vector<std::string> split = splitStringOnNewline(hangman_stages[stage]);
+	for each (string line in split)
+	{
+		cout << 
+	}
 }
 
 void Hangman::startGame() {
-	display();
+	int stage = 0;
+	
+	DataGenerator dg = DataGenerator();
+	std::pair<std::string, std::string> answer = dg.get_random_entry();
 
-	int x;
-	std::cin >> x; // Get user input from the keyboard
+	std::cout << answer.first << std::endl;
+
+	while (stage != MAX_STAGE) {
+		display(stage);
+
+		std::string input;
+		std::cin >> input; // Get user input from the keyboard
+
+		for each (char letter in input) //save the used the letters in the set
+		{
+			seen_letters.insert(letter);
+		}
+
+		if (input.compare(answer.first) != 0) {
+			stage++;
+		}
+		else {
+			printf("Well done!");
+			break;
+		}
+	}
+	
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	
+
 }
+
+std::vector<std::string> splitStringOnNewline(const std::string& input) {
+	std::vector<std::string> lines;
+	std::stringstream ss(input);
+	std::string line;
+
+	// Use std::getline to extract lines
+	while (std::getline(ss, line, '\n')) {
+		lines.push_back(line);
+	}
+
+	return lines;
+}
+};
